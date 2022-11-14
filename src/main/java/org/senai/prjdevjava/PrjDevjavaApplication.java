95680d692facb0b0e45926723535db8138679143
BrunoGBatista <batistabrunog@gmail.com>
2022-11-14 17:16:09 -0300

    commmit0

---
+++ src/main/java/org/senai/prjdevjava/PrjDevjavaApplication.java
@@ -0,0 +1,13 @@
+package org.senai.prjdevjava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrjDevjavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrjDevjavaApplication.class, args);
+	}
+
+}
