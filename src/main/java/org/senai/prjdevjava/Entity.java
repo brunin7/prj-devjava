30ba932a2bd729c2ac81aa27147df0249800d88c
BrunoGBatista <batistabrunog@gmail.com>
2022-11-14 22:00:06 -0300

    Exemplo Professor

---
+++ src/main/java/org/senai/prjdevjava/Entity.java
@@ -0,0 +1,5 @@
+package org.senai.prjdevjava;
+
+public @interface Entity {
+
+}
---
+++ src/main/java/org/senai/prjdevjava/Usuario.Repository.java
@@ -0,0 +1,9 @@
+package org.senai.prjdevjava;
+
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface UsuarioRepository extends CrudRepository <Usuario, Integer> {
+
+
+}
